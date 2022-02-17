00565b7bb4ae2bb0556a83730190617e32e323f2
ZL-Jan-03 <1578093418@qq.com>
2022-02-17 14:54:50 +0800

    add some AEB

--- Evaluate/scripts/AEB4-2.py
+++ Evaluate/scripts/AEB4-2.py
@@ -1,6 +1,6 @@
 # @Author  : 张璐
 # @Time    : 2022/02/17
-# @Function: AEB2-2
+# @Function: AEB4-2
 def get_report(scenario, script_id):
     collision_status_list = scenario.scenario_data['collision_status'].values.tolist()
     # 碰撞
@@ -11,9 +11,9 @@ def get_report(scenario, script_id):
         score = 100
     score_description = '1) 未发生碰撞，得分 100；\n2) 发生碰撞，得分 0。'
     if score == 100:
-        evaluate_item = '自行车向右横穿时未发生碰撞,得100分'
+        evaluate_item = '无遮挡行人向右横穿时未发生碰撞,得100分'
     else:
-        evaluate_item = '自行车向右横穿时发生碰撞,得0分'
+        evaluate_item = '无遮挡行人向右横穿时发生碰撞,得0分'
 
     return {
         'unit_scene_ID': script_id,
