2bd67a1d33604ee00d29be1b4e663d775847d78b
ZL-Jan-03 <1578093418@qq.com>
2022-02-17 13:54:32 +0800

    AEB1-2

--- Evaluate/scripts/AEB1-2.py
+++ Evaluate/scripts/AEB1-2.py
@@ -8,9 +8,9 @@ def get_report(scenario_df, script_id):
         score = 100
     score_description = '1) 未发生碰撞，得分 100；\n2) 发生碰撞，得分 0。'
     if score == 100:
-        evaluate_item = '前方车辆慢行时未发生碰撞,得100分'
+        evaluate_item = '前方车辆刹停时未发生碰撞,得100分'
     else:
-        evaluate_item = '前方车辆慢行时发生碰撞,得0分'
+        evaluate_item = '前方车辆刹停时发生碰撞,得0分'
 
     return {
         'unit_scene_ID': script_id,
