8f2494e2b44a2134fb554888cb03718bbd4336f5
“汤宇飞” <jltyf@sina.com>
2022-03-08 18:19:22 +0800

    添加目标物为空和目标物多个的逻辑

--- Generalization/serialization/scenario_serialization.py
+++ Generalization/serialization/scenario_serialization.py
@@ -1,3 +1,4 @@
+import copy
 import itertools
 
 from enumerations import DataType
@@ -18,31 +19,33 @@ class ScenarioData(object):
         self.scenario_dict['ego_duration_time'] = str(scenario_series['自车轨迹持续时间(s)']).split(',')
         self.scenario_dict['ego_velocity_time'] = eval(scenario_series['自车速度分段持续时间'])
         self.scenario_dict['ego_trajectory_time'] = eval(scenario_series['自车轨迹形态分段持续时间'])
+        self.scenario_dict['generalization_type'] = eval(scenario_series['泛化标志位'])
         # self.scenario_dict['scene_type'] = str(scenario_series['scene_type'])
         # self.scenario_dict['road_type'] = str(scenario_series['road_type'])
         # self.scenario_dict['road_section'] = str(scenario_series['road_section'])
         # self.scenario_dict['scene_description'] = str(scenario_series['scene_description'])
         # self.scenario_dict['test_procedure'] = str(scenario_series['test_procedure'])
         # self.scenario_dict['expect_test_result'] = str(scenario_series['expect_test_result'])
-        self.scenario_dict['obs_start_x'] = str(scenario_series['目标初始x坐标'])
-        self.scenario_dict['obs_start_y'] = str(scenario_series['目标初始y坐标'])
-        self.scenario_dict['obs_start_velocity'] = str(scenario_series['目标初始速度V1(km/h)'])
-        self.scenario_dict['obs_velocity_status'] = str(scenario_series['目标行驶速度状态'])
-        self.scenario_dict['obs_trajectory'] = str(scenario_series['目标轨迹形态']).split(',')
-        self.scenario_dict['obs_duration_time'] = str(scenario_series['目标轨迹持续时间(s)'])
-        self.scenario_dict['obs_trail_time'] = str(scenario_series['目标轨迹形态分段持续时间(s)'])
-        self.scenario_dict['obs_heading_angle_rel'] = str(scenario_series['目标物相对自车航向角'])
-        self.scenario_dict['obs_velocity_time'] = str(scenario_series['目标速度分段持续时间(s)'])
-        # self.scenario_dict['direction'] = str(scenario_series['目标正反向标志']).split(',')[-1]
-        self.scenario_dict['generalization_type'] = eval(scenario_series['泛化标志位'])
-        if self.scenario_dict['scenario_name'] in 'PCW':
-            self.scenario_dict['ped_initial_position_x'] = str(scenario_series['目标初始x坐标']).split(',')
-            self.scenario_dict['ped_initial_position_y'] = str(scenario_series['目标初始y坐标']).split(',')
-            self.scenario_dict['ped_speed_status'] = str(scenario_series['目标行驶速度状态']).split(',')
-            self.scenario_dict['ped_trajectory'] = str(scenario_series['目标轨迹形态']).split(',')
-            self.scenario_dict['ped_speed_duration_time'] = str(scenario_series['目标速度分段持续时间(s)']).split(',')
-            self.scenario_dict['pedT_trail_section_duration_time'] = str(scenario_series['目标轨迹形态分段持续时间(s)']).split(',')
-            self.scenario_dict['ped_heading_angle_rel'] = str(scenario_series['目标物相对自车航向角']).split(',')
+        if str(scenario_series['目标初始x坐标']):
+            self.scenario_dict['obs_start_x'] = str(scenario_series['目标初始x坐标'])
+            self.scenario_dict['obs_start_y'] = str(scenario_series['目标初始y坐标'])
+            self.scenario_dict['obs_start_velocity'] = str(scenario_series['目标初始速度V1(km/h)'])
+            self.scenario_dict['obs_velocity_status'] = str(scenario_series['目标行驶速度状态'])
+            self.scenario_dict['obs_trajectory'] = str(scenario_series['目标轨迹形态']).split(',')
+            self.scenario_dict['obs_duration_time'] = str(scenario_series['目标轨迹持续时间(s)'])
+            self.scenario_dict['obs_trail_time'] = str(scenario_series['目标轨迹形态分段持续时间(s)'])
+            self.scenario_dict['obs_heading_angle_rel'] = str(scenario_series['目标物相对自车航向角'])
+            self.scenario_dict['obs_velocity_time'] = str(scenario_series['目标速度分段持续时间(s)'])
+            # self.scenario_dict['direction'] = str(scenario_series['目标正反向标志']).split(',')[-1]
+            if self.scenario_dict['scenario_name'] in 'PCW':
+                self.scenario_dict['ped_initial_position_x'] = str(scenario_series['目标初始x坐标']).split(',')
+                self.scenario_dict['ped_initial_position_y'] = str(scenario_series['目标初始y坐标']).split(',')
+                self.scenario_dict['ped_speed_status'] = str(scenario_series['目标行驶速度状态']).split(',')
+                self.scenario_dict['ped_trajectory'] = str(scenario_series['目标轨迹形态']).split(',')
+                self.scenario_dict['ped_speed_duration_time'] = str(scenario_series['目标速度分段持续时间(s)']).split(',')
+                self.scenario_dict['pedT_trail_section_duration_time'] = str(scenario_series['目标轨迹形态分段持续时间(s)']).split(
+                    ',')
+                self.scenario_dict['ped_heading_angle_rel'] = str(scenario_series['目标物相对自车航向角']).split(',')
 
     def get_scenario_model(self):
         characteristic_list = list()
@@ -71,13 +74,22 @@ class ScenarioData(object):
                     values_list.append(value)
                 keys_list.append(key)
                 characteristic_list.append(values_list)
-        characteristic_list = list(itertools.product(*characteristic_list))
+        # 只有再多个参数需要泛化的情况下需要做笛卡尔积
+        if len(keys_list) > 1:
+            characteristic_list = list(itertools.product(*characteristic_list))
+        else:
+            characteristic_list = characteristic_list[0]
         for characteristic in characteristic_list:
+            temp_data = self.scenario_dict.copy()
             for key in keys_list:
                 if ';' in self.scenario_dict[key]:
-                    self.scenario_dict[key] = [characteristic[keys_list.index(key)], *other_obj_dict[key]]
+                    temp_data[key] = [str(characteristic[keys_list.index(key)]), *other_obj_dict[key]]
                 else:
-                    self.scenario_generalization_list.append(self.scenario_dict)
+                    if len(keys_list) == 1:
+                        temp_data[key] = str(characteristic)
+                    else:
+                        temp_data[key] = str(characteristic[keys_list.index(key)])
+            self.scenario_generalization_list.append(temp_data)
         for key, values in self.scenario_dict['generalization_type'].items():
             if values == DataType.calculative.value:
                 formula = eval(self.scenario_dict[key])
